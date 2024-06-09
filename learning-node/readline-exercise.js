917b6b7b34ac0e330b3d00264a3eb29ff35d2bc0
brunaportelaa <brunaporteladj@gmail.com>
2024-06-09 13:06:06 -0300

    Separated the collectAnswers function to learn about modules

--- learning-node/readline-exercise.js
+++ learning-node/readline-exercise.js
@@ -1,31 +1,10 @@
-const readline = require('readline')
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+const collectAnswers = require('./lib/collectAnswers')
 
 const questions = [
     "What is the meaning of life",
     "What is yout favorite book?",
     "What is yout favorite pop diva?",
 ]
-
-
-function collectAnswers(questions, done) {
-
-    const answers = []
-    const questionAnswered = answer => {
-        answers.push(answer.trim())
-
-        if(answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered)
-        } else {
-            return done(answers)
-        }
-    }
-    rl.question(questions[0], questionAnswered)
-}
 
 collectAnswers(questions, (answers) => {
     console.log(answers)
